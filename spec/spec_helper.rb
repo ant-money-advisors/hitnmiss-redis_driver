1425d804e4054717101c5c55c74a2ff90d99f8a5
Andrew De Ponte <cyphactor@gmail.com>
2016-06-21 10:50:48 -0700

    Replaced tab character with spaces
    Why you made the change:
    
    I did this for consistency across the project.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require "simplecov"
 SimpleCov.start do
-	add_filter "/spec/"
+  add_filter "/spec/"
 end
 require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start
